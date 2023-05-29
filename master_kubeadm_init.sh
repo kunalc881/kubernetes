843ae1be272239eb23b6945cdfcce88968a08254
kunalc881 <122966979+kunalc881@users.noreply.github.com>
2023-05-29 14:04:34 +0530

    Update master_kubeadm_init.sh

--- master_kubeadm_init.sh
+++ master_kubeadm_init.sh
@@ -11,4 +11,3 @@ systemctl restart containerd
    mkdir -p $HOME/.kube
   sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
   sudo chown $(id -u):$(id -g) $HOME/.kube/config
-  sudo kubeadm token create --print-join-command

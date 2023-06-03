10a1503e6cc2d99ebf4ceb31eeccafc2e6bd42e5
kunalc881 <122966979+kunalc881@users.noreply.github.com>
2023-06-03 11:26:48 +0530

    Update and rename iptables_docker.sh to ubuntu_master.sh

---
+++ ubuntu_master.sh
@@ -0,0 +1,31 @@
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+    sudo modprobe overlay
+    sudo modprobe br_netfilter
+    cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+    sudo sysctl --system
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sh get-docker.sh 
+    systemctl restart docker
+    systemctl enable docker
+    vi /etc/containerd/config.toml
+   systemctl restart containerd
+   systemctl enable containerd
+   sudo apt-get update
+   sudo apt-get install -y apt-transport-https ca-certificates curl
+   curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+   echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+   sudo apt-get update
+   sudo apt-get install -y kubelet kubeadm kubectl
+   sudo kubeadm init
+   mkdir -p $HOME/.kube
+  sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+  sudo chown $(id -u):$(id -g) $HOME/.kube/config
